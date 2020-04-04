4c4ff6f8b85a7eb6191a5f049ded2fea8266a4d0
DMC09 <Dharminlive12@live.com>
2020-04-03 23:40:09 -0500

    display correct object for selected state

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,32 +1,29 @@
 "use strict"
-const btn = document.querySelector('.main');
-const container = document.querySelector('.data__container');
-const statebox = document.querySelector('.state__select')
+const unitedStatesContainer = document.querySelector('.data__container');
+const stateContainer = document.querySelector('.state__container'); //get another container so the select element does not disapear
+
 
 // Gets United State Data on load
-// const getData = (async () => {
-//   try {
-//     container.innerHTML = '<h2 class="loading-text">Loading Data</h2>'
-//     const response = await axios.get('https://thevirustracker.com/free-api?countryTotal=US')
-//     console.log('Data received');
-//     response.data.countrydata.forEach((item) => {
-//       // console.log(item);
-//       container.innerHTML = `
-//       <ul class=data__section>
-//       <li class="orange-text">${item.total_cases} Total Cases</li>
-//       <li class="green-text">${item.total_recovered}  Recovered</li>
-//       <li class="red-text">${item.total_deaths} Deaths </li>
-//       <li class="yellow-text">${item.total_new_cases_today} New Cases Today</li>
-//       </ul>
-//         `
-//       console.log('Data loaded');;
-//     });
-//
-//   } catch (error) {
-//     console.error(error)
-//     container.innerHTML = `<h3 class="error-text"> API data unable to load please try again later</h3>`
-//   }
-// })();
+const getData = (async () => {
+  try {
+    unitedStatesContainer.innerHTML = '<h2 class="loading-text">Loading Data</h2>'
+    const response = await axios.get('https://api.thevirustracker.com/free-api?countryTotal=US')
+    response.data.countrydata.forEach((item) => {
+      unitedStatesContainer.innerHTML = `
+      <ul class=data__section>
+      <li class="orange-text">${item.total_cases} Total Cases</li>
+      <li class="green-text">${item.total_recovered}  Recovered</li>
+      <li class="red-text">${item.total_deaths} Deaths </li>
+      <li class="yellow-text">${item.total_new_cases_today} New Cases Today</li>
+      </ul>
+        `
+    });
+
+  } catch (error) {
+    console.error(error)
+    container.innerHTML = `<h3 class="error-text"> API data unable to load please try again later</h3>`
+  }
+})();
 
 
 
@@ -42,31 +39,35 @@ $(document).ready(function() {
     width: '75%'
   });
   $('#state__select').on('select2:select', function getSelect(e) {
+    const name = e.params.data.text;
     const id = e.params.data.id;
-    stateData(id);
+    stateData(id,name);
   })
 });
 // function to retreive ID from dropdown and match with state data
-const stateData = async (id) => {
+const stateData = async (id,name) => {
   try {
-    const response = await axios.get('https://covidtracking.com/api/states')
-    const statelist = response.data;
-    const stateInfo = statelist.filter(function listGet(e) {
-      return e.state === id; // we can just use "id" because it's already in our scope
-    });
-    const state = stateInfo[0]
-console.log(state.death);
-console.log(state.positive);
-console.log(state);
-console.log(state);
+    const response = await axios.get(`https://covidtracking.com/api/states?state=${id}`)
+    const {...stateStats} = response.data;
+    console.log(stateStats.state); // abbreviation of state
+    console.log(name); // name of state
+    console.log(stateStats); //  state obj
+    console.log(stateStats.death); //  deaths in state
+    console.log(stateStats.totalTestResults); // Total tested in state
+    console.log(stateStats.positive); // Positive cases
+    console.log(stateStats.hospitalized || 0); // # of hospitalized
 
+stateContainer.innerHTML = `
+<ul>
+<li>${name} State</li>
+<li>${stateStats.state} State abreviation</li>
+<li>${stateStats.death} Deaths </li>
+<li>${stateStats.totalTestResults} Total tested</li>
+<li>${stateStats.positive} positives</li>
+<li>${stateStats.hospitalized || 0} hospitalized</li>
+</ul>
 
-
-
-    // const stateInfo = statelist.filter(function listGet(e, id) {
-    //   return e.state === "TX";
-    // });
-    // console.log(stateInfo[0]);
+`
   } catch (error) {
     console.error(error)
   }
