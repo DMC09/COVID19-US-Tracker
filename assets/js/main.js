bac631e07b92fe9f0c5cbbed0d4c597d448e6aa2
DMC09 <Dharminlive12@live.com>
2020-07-24 18:07:13 -0500

    seperated numbers and title

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,40 +8,40 @@ const toFormat = (number) =>
     : number.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,");
 
 // Gets United State Data on load
-const getUSData = (async () => {
-  const loadingText =
-    '<div class="lds-grid"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>';
-  const errorText = `<h3 class="error-text"> API data unable to load please try again later</h3>`;
-  const api = "https://api.thevirustracker.com/free-api?countryTotal=US";
-  try {
-    unitedStatesContainer.innerHTML = loadingText;
-    const response = await axios.get(api);
-    console.log(response);
-    console.log(response.data);
-    const { countrydata } = response.data;
-    console.log(countrydata);
-
-    countrydata.forEach((item) => {
-      const {
-        total_cases,
-        total_recovered,
-        total_deaths,
-        total_new_cases_today,
-      } = item;
-      unitedStatesContainer.innerHTML = `
-      <ul class=united-data__api>
-      <li><span class="num">${toFormat(total_cases)}</span> <span class="data-type">Total Cases</span></li>
-      <li><span class="num">${toFormat(total_recovered)}</span>  Recovered</li>
-      <li><span class="num">${toFormat(total_deaths)}</span> Deaths </li>
-      <li><span class="num">${toFormat(total_new_cases_today)}</span> New Cases Today</li>
-      </ul>
-        `;
-    });
-  } catch (error) {
-    console.error(error);
-    // unitedStatesContainer.innerHTML = errorText;
-  }
-})();
+// const getUSData = (async () => {
+//   const loadingText =
+//     '<div class="lds-grid"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>';
+//   const errorText = `<h3 class="error-text"> API data unable to load please try again later</h3>`;
+//   const api = "https://api.thevirustracker.com/free-api?countryTotal=US";
+//   try {
+//     unitedStatesContainer.innerHTML = loadingText;
+//     const response = await axios.get(api);
+//     console.log(response);
+//     console.log(response.data);
+//     const { countrydata } = response.data;
+//     console.log(countrydata);
+//
+//     countrydata.forEach((item) => {
+//       const {
+//         total_cases,
+//         total_recovered,
+//         total_deaths,
+//         total_new_cases_today,
+//       } = item;
+//       unitedStatesContainer.innerHTML = `
+//       <ul class=united-data__api>
+//       <li><span class="num">${toFormat(total_cases)}</span> <span class="data-type">Total Cases</span></li>
+//       <li><span class="num">${toFormat(total_recovered)}</span>  Recovered</li>
+//       <li><span class="num">${toFormat(total_deaths)}</span> Deaths </li>
+//       <li><span class="num">${toFormat(total_new_cases_today)}</span> New Cases Today</li>
+//       </ul>
+//         `;
+//     });
+//   } catch (error) {
+//     console.error(error);
+//     // unitedStatesContainer.innerHTML = errorText;
+//   }
+// })();
 
 // Function to in Select dropdown and retreive selection
 $(document).ready(function () {
